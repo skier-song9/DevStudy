6f32e91e08078c55df265742f5a2fa1a819a7f6c
song9-LENOVO <0603paul@gmail.com>
2023-03-20 18:06:09 +0900

    23.03.20 - update

---
+++ C++/kmuproj/lab/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+
+    int a,b;
+    int c=1,d=10;
+    a=0,b=0;
+    cout << "test1: " << a+b+c+d ;
+
+    int y=0, double x=1.1;
+    cout << "test2: " << x ;
+
+    return 0;
+}
